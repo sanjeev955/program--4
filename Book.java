1204096e841a4b37ae4a41420a243668768f768c
sanjeev955 <sanjeevs13711@gmail.com>
2025-05-27 12:35:27 +0530

    save

---
+++ Book.java
@@ -0,0 +1,28 @@
+package today272.today273;
+
+public class Book {
+    private String bookName, author, publisher;
+    private double price;
+    private int yearOfPublished;
+
+    public Book(String bookName, String author, String publisher, double price, int yearOfPublished) {
+        this.bookName = bookName;
+        this.author = author;
+        this.publisher = publisher;
+        this.price = price;
+        this.yearOfPublished = yearOfPublished;
+    }
+
+    public void display() {
+        System.out.println("Book Name: " + bookName);
+        System.out.println("Author: " + author);
+        System.out.println("Publisher: " + publisher);
+        System.out.println("Price: $" + price);
+        System.out.println("Year of Published: " + yearOfPublished);
+    }
+
+    public static void main(String[] args) {
+        Book book = new Book("Java Programming", "John Doe", "Tech Press", 49.99, 2022);
+        book.display();
+    }
+}
